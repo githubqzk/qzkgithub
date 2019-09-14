e3e83a8228a7fa623bbf1e1e5cea217257c85a1e
githubqzk <920874915@qq.com>
2019-09-14 20:00:12 +0800

    new cmake-build-debug/

---
+++ cmake-build-debug/CMakeFiles/qzkgithub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qzkgithub.dir/main.cpp.o"
+  "qzkgithub.pdb"
+  "qzkgithub"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/qzkgithub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
